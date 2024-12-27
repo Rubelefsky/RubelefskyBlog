2c09aa0ac892e2ff6960a799148260b24c04ec11
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2024-12-27 10:04:07 -0500

    Blog Pages, Index page, CSS styles and JS script
    Using Tailwinds CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-custom': 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(29,56,253,1) 50%, rgba(204,69,252,1) 100%)',
+            },
+        }
+    }
+}
